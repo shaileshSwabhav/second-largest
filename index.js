06a81c9ba53d719ff3cc86bc8692125614be3e8e
shaileshSwabhav <shailesh@swabhavtechlabs.com>
2023-04-08 10:11:40 +0530

    commit 1

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.post('/second-largest', (req, res) => {
   const { arr } = req.body;
   console.log("/second-largest route hit", arr);
 
-  let maxNumber = 0
-  let secondMaxNumber = 0
+  let maxNumber = arr[0]
+  let secondMaxNumber = arr[1]
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > maxNumber) {
